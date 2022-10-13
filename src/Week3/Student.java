efd57a8ec2fd25fc7d519b37937c083ac1acf348
niccbebb <nicolecbebb@gmail.com>
2022-10-13 15:38:54 +0100

    Created a Student class

---
+++ src/Week3/Student.java
@@ -0,0 +1,19 @@
+package Week3;
+
+public class Student 
+{
+    private int id;
+    private String name;
+
+    public Student(int id, String name)
+    {
+        this.id = id;
+        this.name = name;
+    }
+
+    public void print()
+    {
+    System.out.println("ID: " + id);
+    System.out.println("Name: " + name);
+    }
+}
--- src/Week3/Week03.java
+++ src/Week3/Week03.java
@@ -15,6 +15,9 @@ public class Week03
         System.out.println();
         System.out.println(" by Nicholas Day and Derek Peacock");
         System.out.println();
+
+        Student Nic = new Student(22044097, "Nic");
+        Nic.print();
     }     
     
 }
